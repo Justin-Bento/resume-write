98e5ab7110e6cbd7d85c6b0089edef20b5e7cfa7
Justin Bento <bento.a.justin@gmail.com>
2024-05-18 01:38:57 -0400

    Extended color to include the chicago gray scale.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+         'chicago': {
+          50: "#F5F6F6",
+          100: "#E6E7E7",
+          200: "#CFD2D2",
+          300: "#AEB2B1",
+          400: "#868A88",
+          500: "#6B6F6D",
+          600: "#595D5C",
+          700: "#4D5150",
+          800: "#444645",
+          900: "#444645",
+          950: "#252727",
+         }
+        //...
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         grotesk: ['Space Grotesk', ...defaultTheme.fontFamily.sans]
